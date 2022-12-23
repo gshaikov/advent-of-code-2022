c6c73dbd9e0b3d0626253c26d9ca82ca11b7cf24
George Shaikovski <g.shaikov@gmail.com>
2022-12-23 00:16:04 +0000

    day 2 b

--- 2/main.cpp
+++ 2/main.cpp
@@ -11,24 +11,39 @@ using std::vector;
 
 // 0 - Rock, 1 - Paper, 2 - Scissors
 static const map<char, int> THEIR_HAND{{'A', 0}, {'B', 1}, {'C', 2}};
-static const map<char, int> OUR_HAND{{'X', 0}, {'Y', 1}, {'Z', 2}};
+
+// Outcomes
+static const map<char, int> OUTCOME{{'X', 0}, {'Y', 3}, {'Z', 6}};
 
 // Game scores, rows - them, cols - us
-static const int SCORES[3][3]{{3, 6, 0}, {0, 3, 6}, {6, 0, 3}};
+static const int GAME_SCORE[3][3]{{3, 6, 0}, {0, 3, 6}, {6, 0, 3}};
 
 // Hand scores
 static const map<char, int> HAND_SCORE{{0, 1}, {1, 2}, {2, 3}};
 
-int compute_score(std::istream &s)
+int solve(std::istream &s)
 {
     int score = 0;
     for (string line; std::getline(s, line);)
     {
         auto their_hand = THEIR_HAND.at(line[0]);
-        auto our_hand = OUR_HAND.at(line[2]);
+        auto outcome = OUTCOME.at(line[2]);
+
+        auto response_scores = GAME_SCORE[their_hand];
+
+        int our_hand = 0;
+        for (; response_scores[our_hand] != outcome; ++our_hand)
+        {
+            if (our_hand > 2)
+            {
+                throw std::out_of_range("Cannot find respose score that matches outcome.");
+            }
+        }
+
+        score += outcome;
         score += HAND_SCORE.at(our_hand);
-        score += SCORES[their_hand][our_hand];
     }
+    std::cout << "Score: " << score << '\n';
     return score;
 }
 
@@ -43,10 +58,8 @@ int main(int argc, char *argv[])
     if (arg == "test")
     {
         string s = "A Y\nC Z\nB X";
-        std::stringstream ss;
-        ss.str(s);
-        int score = compute_score(ss);
-        assert(score == (2 + 6 + 3 + 3 + 1 + 0));
+        std::istringstream ss{s};
+        assert(solve(ss) == (1 + 3 + 1 + 6 + 1 + 0));
     }
     else
     {
@@ -56,8 +69,7 @@ int main(int argc, char *argv[])
             std::cout << "Cannot open input file." << EOF;
             return 1;
         }
-        int score = compute_score(f);
-        std::cout << "Score: " << score << '\n';
+        solve(f);
         f.close();
     }
 
